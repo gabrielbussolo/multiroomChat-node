daf1966e1edaaef01b5845a5334bcd0fe4f82604
Gabriel Ferreira <ferreira@MacBook-Pro-de-Gabriel.local>
2018-03-14 08:35:34 -0300

    Trabalhando com socket.io, implementando msg inicial

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 const app = require('./config/server')
 
 //parametrização da porta de escuta
-app.listen(3000, () => {
+let server = app.listen(3000, () => {
     console.log('Servidor ON!')
+})
+
+let io = require('socket.io').listen(server)
+app.set('io', io)
+
+//conexao websocket
+io.on('connection', (socket)=> {
+    console.log('Usuario conectou')
+    socket.on('disconnect', ()=>{
+        console.log('usuario desconectou')
+    })
 })
